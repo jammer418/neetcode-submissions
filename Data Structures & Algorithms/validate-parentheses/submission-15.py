d2ba48b23aef71981a98894da015a986e94fd19a
jammer418 <65597772+jammer418@users.noreply.github.com>
2026-09-10 14:27:20 -0400

    Add: validate-parentheses - submission-15

---
+++ Data Structures & Algorithms/validate-parentheses/submission-15.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        str = s 
+        opn_brak = "({["
+        cls_brak = ")}]"
+        temp_stack = []
+
+        for char in s:
+            o_index = None
+            c_index = None
+            if char in opn_brak:
+                temp_stack.append(char)
+            else:
+                if len(temp_stack) == 0:
+                    return False
+                temp_var = temp_stack.pop()
+                o_index = opn_brak.find(temp_var)
+                c_index = cls_brak.find(char)
+            if o_index != c_index:
+                return False
+        if not temp_stack:
+            return True
+        else:
+            return False
+           
+        
+        
